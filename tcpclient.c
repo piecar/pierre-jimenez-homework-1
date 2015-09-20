045882ad3c2b8f1e18c5e7bc1cf63575422702bd
Pierre <pjime032@fiu.edu>
2015-09-20 16:54:36 -0400

    Added lslocal, lsremote, exit for client
    Begin get and put for client

--- tcpclient.c
+++ tcpclient.c
@@ -12,8 +12,8 @@ void syserr(char* msg) { perror(msg); exit(-1); }
 int main(int argc, char* argv[])
 {
   //Sockfd: file descriptor; portno: port number; n: return values for read and write
-  int sockfd, portno, n;
-  char *input;
+  int sockfd, portno, n, size, tempfd;
+  char *input, *file; //user input, file pointer
   struct hostent* server; // server info
   struct sockaddr_in serv_addr; //server address info
   char buffer[256];
@@ -28,18 +28,6 @@ int main(int argc, char* argv[])
     return 2;
   }
   portno = atoi(argv[2]);
-  
-  /* To display host name
-  {
-  struct in_addr **addr_list; int i;
-  printf("Official name is: %s\n", server->h_name);
-  printf("    IP addresses: ");
-  addr_list = (struct in_addr **)server->h_addr_list;
-  for(i = 0; addr_list[i] != NULL; i++) {
-    printf("%s ", inet_ntoa(*addr_list[i]));
-  }
-  printf("\n");
-  }*/
 
   sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // check man page
   if(sockfd < 0) syserr("can't open socket");
@@ -60,17 +48,47 @@ int main(int argc, char* argv[])
   	scanf("%s", &input);
   	if(strcmp(input, "ls-local") == 0)
   	{
+		system("ls");
   	}
   	if(strcmp(input, "ls-remote") == 0)
   	{
+		strcpy(buffer, "ls");
+		n = send(sockfd, buffer, strlen(buffer), 0); // may need to fix buffer with /0 ending
+		if(n < 0) syserr("can't send to server");
+		n = recv(sockfd, &size, sizeof(int), 0);
+        if(n < 0) syserr("can't receive from server");
+		file = malloc(size); // make space for a list of files on the server
+		
+		n = recv(sockfd, file, sizeof(file), 0); //put list into f
+		tempfd = creat("remotelist.txt", O_WRONLY); //create  a text file to put list
+		write(tempfd, &file, size);
+		close(tempfd);
+		printf("Files at the server(%s)\n", argv[1]);
+		system("cat remotelist.txt");
+		free(file);		
   	}
   	if(strcmp(input, "exit") == 0)
   	{
+		strcpy(buffer, "exit");
+		n = send(sockfd, buffer, strlen(buffer), 0); // may need to fix buffer with /0 ending
+		if(n < 0) syserr("can't send to server");
+		n = recv(sockfd, &size, sizeof(int), 0);
+        if(n < 0) syserr("can't receive from server");
+		if(size)
+		{
+			exit(0);
+		}
+		else
+		{
+			printf("Server didn't exit");
+		}
+		
   	} 	
   	//tokenize if get/put
   	char *comm, filename;
   	comm = strtok(input, " ");
   	filename = strtok(NULL, " ");
+	
   	if(strcmp(input, "get") == 0)
   	{
   		strcpy(buffer, "get");
@@ -78,11 +96,13 @@ int main(int argc, char* argv[])
   	}
   	if(strcmp(input, "put") == 0)
   	{
+  		strcpy(buffer, "put");
+  		strcat(buffer, filename);
   	}
   	
   }
   
-
+/*
   printf("PLEASE ENTER MESSAGE: ");
   fgets(buffer, 255, stdin);
   n = strlen(buffer); if(n>0 && buffer[n-1] == '\n') buffer[n-1] = '\0';
@@ -95,7 +115,7 @@ int main(int argc, char* argv[])
   if(n < 0) syserr("can't receive from server");
   else buffer[n] = '\0';
   printf("CLIENT RECEIVED MESSAGE: %s\n", buffer);
-
+*/
   close(sockfd);
   return 0;
 }
